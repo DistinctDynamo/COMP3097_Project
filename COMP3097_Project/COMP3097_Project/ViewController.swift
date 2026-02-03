9266143ab93368d9fca5b2dd92cd410a3a0bb67f
Tech <tech@Techs-iMac-8.local>
2026-02-03 18:48:28 -0500

    first commit

---
+++ COMP3097_Project/COMP3097_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COMP3097_Project
+//
+//  Created by Tech on 2026-02-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
