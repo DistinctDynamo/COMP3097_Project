77f03b8c7deef74c1305e459886ec7c4e8ff13af
Steven Nguyen <nguyensteven578@gmail.com>
2026-03-10 21:24:07 -0400

    working on adding categories

---
+++ COMP3097_Project/COMP3097_Project/addCategoryController.swift
@@ -0,0 +1,37 @@
+//
+//  addCategoryController.swift
+//  COMP3097_Project
+//
+//  Created by Tech on 2026-03-10.
+//
+
+import UIKit
+
+class addCategoryController: UIViewController {
+
+    var categoryName:String = ""
+    @IBOutlet weak var categoryNameLabel: UILabel!
+    @IBOutlet weak var categoryNameField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func addCategoryAction(_ sender: Any) {
+        self.dismiss(animated: true){
+            self.categoryName = self.categoryNameField.text ?? ""
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
